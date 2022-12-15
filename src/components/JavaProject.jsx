09faf13355e4ef81ef4d4bb6a868164ebafb9542
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-12-15 11:28:49 +0000

    fixing merge again

--- src/components/JavaProject.jsx
+++ src/components/JavaProject.jsx
@@ -4,13 +4,6 @@ import './JavaProject.css'
 
 const JavaProject = () => {
   return (
-<<<<<<< HEAD
-    <center>
-    <div className='videoDiv'>
-      <iframe className='video' src="https://player.vimeo.com/video/772384082?h=87f7144e9e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" title="YouTube video player" border="0px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-    </div>
-    </center>
-=======
     <>
       <div className='videoDiv'>
         <iframe className='video' src="https://player.vimeo.com/video/772384082?h=87f7144e9e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" title="YouTube video player" border="0px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
@@ -37,7 +30,6 @@ const JavaProject = () => {
         <p>I created an eye-catching GUI with images that change according to player encounters. It also features many puzzles to solve and a combat system with game logic.</p>
     </div>
     </>
->>>>>>> e586a8fab094234c51203b5d62106647330949ac
   )
 }
 
