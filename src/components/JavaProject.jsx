111615ae538d5e1cad895fb7b1f0671626176f49
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-12-15 11:25:22 +0000

    fixing merge

--- src/components/JavaProject.jsx
+++ src/components/JavaProject.jsx
@@ -4,9 +4,11 @@ import './JavaProject.css'
 
 const JavaProject = () => {
   return (
-    <div>
-        <h2>Under Construction</h2>
+    <center>
+    <div className='videoDiv'>
+      <iframe className='video' src="https://player.vimeo.com/video/772384082?h=87f7144e9e&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" title="YouTube video player" border="0px" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
     </div>
+    </center>
   )
 }
 
--- src/components/MyCV.jsx
+++ src/components/MyCV.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './MyCV.css'
-import davidvtaylor from './davidvtaylor.pdf'
 
 
 const MyCV = () => {
@@ -8,7 +7,7 @@ const MyCV = () => {
 
   return (
     <div className='cv'>
-      <iframe src={`${davidvtaylor}#view=fitH&toolbar=0&navpanes=0`}/>
+      
     </div>
   )
 }
